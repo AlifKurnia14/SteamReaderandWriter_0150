510d4193b74cf97a80c75c4c61d9eaf7dc192aa2
AlifKurnia14 <tsimpsons056@gmail.com>
2025-06-10 18:29:36 +0700

    menutup file dengan. infile.close

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -35,8 +35,12 @@ int main(){
     if (infile.is_open()){
         // melakukan perulangan setiap baris
         while (getline(infile, baris )){
-            
+            // dan tampilkan disini
+            cout << baris << '\n';
+
         }
+        // tutup file tersebut setelah selesai
+        infile.close();
     }
 
 
