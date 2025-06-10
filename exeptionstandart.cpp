4eb24ad778a9fc33f852f356673dfd0296c2e864
AlifKurnia14 <tsimpsons056@gmail.com>
2025-06-10 18:37:40 +0700

    pesan aaray integer 3 elemen

--- exeptionstandart.cpp
+++ exeptionstandart.cpp
@@ -2,3 +2,14 @@
 #include <exception>
 // untuk obyek exception yg akan digunakan 
 #include <array>
+// untuk obyek array yg akan kita gunakan;
+using namespace std;
+
+int main(){
+    cout << "Awal Program "<< endl;
+    try {
+        array <int , 3> data = {10, 20, 30 };
+        // pesan array integer 3 elemen 
+        
+    }
+}
